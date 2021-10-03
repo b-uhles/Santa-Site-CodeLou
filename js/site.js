52058edd76342ebe12bdc143c9810f3d00b3fcb2
Brandon <brandon.uhles375@gmail.com>
2021-10-03 15:53:00 -0400

    Built navbar, should flex from horizontal to vertical on mobile.

--- js/site.js
+++ js/site.js
@@ -1,1 +1,9 @@
-alert ("Javascript Operational.")
+/*referenced by CSS to add and remove the responsive class for mobile support. */
+function addRespClass() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
